3cb607404f72d00686db810e25f5bb1826d9c8d5
Durere <miruru2003@gmail.com>
2022-09-20 16:39:59 +0300

    gata in sfarsit
    doamne ajuta (multumesc ivan :* )

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robot.java
@@ -3,24 +3,17 @@ package org.firstinspires.ftc.teamcode.drive;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.drive.subsystems.MecanumDriveCh;
-import org.firstinspires.ftc.teamcode.drive.subsystems.Carousel;
-import org.firstinspires.ftc.teamcode.drive.subsystems.Sweeper;
 
 public class Robot {
     private  boolean initialize;
-    public Sweeper sweeper;
-    public Carousel carousel;
     public MecanumDriveCh drive;
 //    public Odometrie odometrie;
 
     public Robot (HardwareMap hardwareMap){
         initialize = true;
-        sweeper = new Sweeper(hardwareMap);
-        carousel = new Carousel(hardwareMap);
         drive = new MecanumDriveCh(hardwareMap);
 //        odometrie = new Odometrie(hardwareMap);
         initialize = false;
     }
     public boolean isInitialize() {return initialize;}
 }
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LinearDriveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LinearDriveMode.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.drive.Robot;
 
 
@@ -13,7 +14,7 @@ import org.firstinspires.ftc.teamcode.drive.Robot;
 public class LinearDriveMode extends LinearOpMode {
     private Robot robot = null;
     private BNO055IMU imuV2;
-
+    
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData(">", "Initializing...");
@@ -37,29 +38,6 @@ public class LinearDriveMode extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            telemetry.addData("Encoder ticks: ", robot.drive.mo)
-
-            if(gamepad2.a) robot.sweeper.levelOne();
-            if(gamepad2.x) robot.sweeper.levelTwo();
-            if(gamepad2.y) robot.sweeper.levelThree();
-
-            if(gamepad2.dpad_up) robot.sweeper.bratUp();
-            if(gamepad2.dpad_down) robot.sweeper.bratDown();
-
-            if(gamepad2.left_bumper) robot.sweeper.outtake();
-            else if(gamepad2.right_bumper) robot.sweeper.intake();
-            else robot.sweeper.stopSweep();
-
-            if(gamepad2.right_stick_button) robot.sweeper.sweepIn();
-            else if(gamepad2.left_stick_button) robot.sweeper.sweepOut();
-
-            if(gamepad1.right_bumper) robot.sweeper.culcatCuva();
-            if(gamepad1.x) robot.sweeper.resetCuva();
-
-//            Carousel:
-            if(gamepad1.left_bumper) robot.carousel.startCarousel();
-            else if(gamepad1.dpad_up) robot.carousel.startCarouselReverse();
-            else robot.carousel.stopCarousel();
 
 //            Drive:
             robot.drive.setDrivePower(new Pose2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x));
