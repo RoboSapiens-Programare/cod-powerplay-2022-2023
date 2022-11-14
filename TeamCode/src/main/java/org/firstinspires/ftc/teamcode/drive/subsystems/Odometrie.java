5769b662d550c9e352e86f4f4f12ddd079a05d0e
RobosapiensProgramare <robosapienscnshb@gmail.com>
2022-11-14 15:41:51 +0200

    glisiera

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/subsystems/Odometrie.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.drive.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+public class Odometrie {
+    public DcMotorEx Odometrie;
+    public Odometrie(HardwareMap hardwareMap){
+        Odometrie = (DcMotorEx) hardwareMap.dcMotor.get("Odometrie");
+        Odometrie.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Odometrie.setDirection(DcMotorSimple.Direction.FORWARD);
+        Odometrie.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void setPower(double power){
+        Odometrie.setPower(0);
+    }
+}
+
